03e84490ccca32cb849afe4c955b9163a86c7305
mclinch <michael.clinch@gmail.com>
2017-05-06 14:15:20 -0700

    final1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,46 +3,54 @@ $(document).ready(function(){
   var randomNumber = (Math.floor(Math.random() * 102) + 19);
 
   $('.ranNum').html(randomNumber);
-
+  var scoreTotal = 0;
+  var wins = 0;
+  var losses = 0; 
   var rubyNum = (Math.ceil(Math.random() * 12));
   var diamondNum = (Math.ceil(Math.random() * 12));
   var amberNum = (Math.ceil(Math.random() * 12));
   var emeraldNum = (Math.ceil(Math.random() * 12));
 
   function reset() {
+    randomNumber = (Math.floor(Math.random() * 102) + 19);
     $('.ranNum').html(randomNumber);
     var rubyNum = (Math.ceil(Math.random() * 12));
     var diamondNum = (Math.ceil(Math.random() * 12));
     var amberNum = (Math.ceil(Math.random() * 12));
     var emeraldNum = (Math.ceil(Math.random() * 12)); 
-    var scoreTotal = 0;
+    scoreTotal = 0;
+    $('#userTotal').html(scoreTotal);
+    
   }
   
   function game() {
 
-  var scoreTotal = 0;
-  var wins = 0;
-  var losses = 0;  
+ 
 
   $('#ruby').click(function(){
     scoreTotal = scoreTotal + rubyNum;
     $('#userTotal').html(scoreTotal);
+    check();
+
   });
   
   $('#diamond').click(function(){
     scoreTotal = scoreTotal + diamondNum;
     $('#userTotal').html(scoreTotal);
+    check();
   });
   
   $('#amber').click(function(){
     scoreTotal = scoreTotal + amberNum;
     $('#userTotal').html(scoreTotal);
+    check();
   });
   
   $('#emerald').click(function(){
     scoreTotal = scoreTotal + emeraldNum;
     $('#userTotal').html(scoreTotal);
     console.log(scoreTotal);
+    check();
   });
 
     check();
@@ -52,6 +60,8 @@ $(document).ready(function(){
         // } 
 
   function check() {
+    console.log('randomNumber ' +  randomNumber);
+    console.log('scoreTotal ' +  scoreTotal);
         if (randomNumber === scoreTotal) {
             wins++;
             $('#winCount').html(wins);
@@ -59,7 +69,7 @@ $(document).ready(function(){
             reset();
         } else if (randomNumber < scoreTotal) {
             losses++;
-            $('lossCount').html(losses);
+            $('#lossCount').html(losses);
             $('#youWin').html("YOU WENT OVER :(");
             reset();
           }
